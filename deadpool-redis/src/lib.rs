c6b29fae345fdd63432c5ba5d0db41824d2f3868
honhimW <honhimw@outlook.com>
2024-09-19 18:48:38 +0800

    update: Cargo.toml

--- deadpool-redis/src/lib.rs
+++ deadpool-redis/src/lib.rs
@@ -173,6 +173,7 @@ impl managed::Manager for Manager {
     }
 }
 
+/// Pretty format human readable redis command.
 pub fn format_cmd(cmd: &Cmd) -> String {
     let mut s = String::new();
     for x in cmd.args_iter() {
