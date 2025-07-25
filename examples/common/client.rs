4ca633625e4d52703b524f55faa06f511bf120ba
honhimW <honhimw@outlook.com>
2025-07-25 09:31:29 +0800

    misc: disable logger output_line & examples client info read from env

--- examples/common/client.rs
+++ examples/common/client.rs
@@ -1,15 +1,28 @@
+use std::env;
+use std::env::VarError;
 use anyhow::{Context, Result};
 use deadpool_redis::Runtime;
 use deadpool_redis::redis::ConnectionAddr::Tcp;
 use deadpool_redis::redis::{ConnectionInfo, ProtocolVersion, RedisConnectionInfo};
 
 pub fn dead_pool() -> Result<deadpool_redis::Pool> {
+    let host = env::var("REDIS_HOST").unwrap_or("127.0.0.1".to_string());
+    let port = env::var("REDIS_PORT").unwrap_or("6379".to_string());
+    let username = match env::var("REDIS_USERNAME") {
+        Ok(p) => Some(p),
+        Err(_) => None,
+    };
+    let password = match env::var("REDIS_PASSWORD") {
+        Ok(p) => Some(p),
+        Err(_) => None,
+    };
+    let db = env::var("REDIS_DB").unwrap_or("0".to_string());
     build_pool(Config {
-        host: "beta.vm.internal".to_string(),
-        port: 6380,
-        username: None,
-        password: Some("123456".to_string()),
-        db: 0,
+        host,
+        port: port.parse()?,
+        username,
+        password,
+        db: db.parse()?,
         protocol: ProtocolVersion::RESP3,
     })
     // build_pool(Config {
